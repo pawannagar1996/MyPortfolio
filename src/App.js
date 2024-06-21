70deffa1898f443caf48199040e3bd540e1185fd
pawannagar1996 <pawannagar1996@gmail.com>
2024-06-21 19:31:42 +0530

    responsive issue solve

--- src/App.js
+++ src/App.js
@@ -3,33 +3,9 @@ import Magnative from "./components/magnativejs/Magnative";
 import "./App.css";
 
 function App() {
-
-// let animationCurser2 = document.querySelector(".animationCurser2")
-
-// console.log(animationCurser2.innerHTML = "pawan")
-
-//   function myFunction(windowWidth) {
-//     if (windowWidth.matches) { // If media query matches
-//       document.body.style.backgroundColor = "yellow";
-//     } else {
-//       document.body.style.backgroundColor = "pink";
-//     }
-//   }
-// // Create a MediaQueryList object
-// let windowWidth = window.matchMedia("(max-width: 768px)")
-
-// // Call listener function at run time
-// myFunction(windowWidth);
-
-// // Attach listener function on state changes
-// windowWidth.addEventListener("change", function() {
-//   myFunction(windowWidth);
-// });
-
   return (
     <div className="App">
-       <AnimatedCursorWindow />
-
+       <span className="animateCursor"><AnimatedCursorWindow /></span>
       <Magnative />
     </div>
   );
